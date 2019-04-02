a5bb3176d61935688915bb87e064765acf87637b
Oleg Averkin <oleg.averkin@lookout.com>
2019-04-02 13:12:08 +0300

    Add fibonacci function and simple tests

---
+++ test_fib.py
@@ -0,0 +1,17 @@
+from fibonacci import generateFibonacci
+
+
+def test_fibonacci():
+    assert generateFibonacci(1) == [1]
+
+
+def test_fibonacci_2():
+    assert [1, 1, 2, 3, 5, 8, 13, 21] == generateFibonacci(8)
+
+
+def test_fibonacci_length():
+    assert len(generateFibonacci(2)) == 2
+
+
+def test_fibonacci_negative_number():
+    assert generateFibonacci(-12) == []
